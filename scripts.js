6e0b141c372aef5a271980190d61faa805a926b1
leonardohernandes <lequinhoh@hotmail.com>
2021-02-15 23:27:47 -0300

    Morte da cobrinha e alert de game-over

--- scripts.js
+++ scripts.js
@@ -50,7 +50,7 @@ function iniciarJogo(){
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert("Se Fodeu! :(");
+            alert("Game Over! :(");
         }
     }
 
